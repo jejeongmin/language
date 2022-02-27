c19121e2ac86b52a98598f85911b11ad378c6160
jackal <jjmyann@hanmail.net>
2022-02-28 01:08:01 +0900

    Add files via upload

---
+++ chater8_parameter_binding_example.ps1
@@ -0,0 +1,10 @@
+﻿[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True)]
+    [string]$ComputerName,
+    [int]$EventID = 4634
+)
+
+Get-EventLog -LogName Security -ComputerName $ComputerName | 
+Where-Object EventID -eq $EventID |
+Select-Object -First 10
