c13283c1ea1874b9bef06bb49ee9c9ce7bbf49dd
jackal <jjmyann@hanmail.net>
2022-03-13 20:21:20 +0900

    제어문 문법 예제 업로드

---
+++ example_control_statement/example_for_continue_01.ps1
@@ -0,0 +1,9 @@
+﻿$ProcessList = Get-Process
+ForEach($Process in $ProcessList)
+{
+    if(($Process.PM/1MB) -le 250)
+    {
+        continue
+    }
+    Write-Output ($Process.Name+'은 250MB 이상의 메모리를 사용한다')
+}
