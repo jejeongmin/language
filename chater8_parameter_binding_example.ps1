4b56c3dae865f42ce8608be0295bc5d53a25dae7
jackal <jjmyann@gmail.com>
2022-03-19 08:32:23 +0900

    write-debug 용례 추가

--- chater8_parameter_binding_example.ps1
+++ chater8_parameter_binding_example.ps1
@@ -1,9 +1,17 @@
-﻿[CmdletBinding()]
+[CmdletBinding()]
 Param(
     [Parameter(Mandatory=$True)]
     [string]$ComputerName,
     [int]$EventID = 4634
 )
+
+# 디버깅 메시지 출력
+Write-Debug -Message "`$ComputerName: $ComputerName"
+Write-Verbose -Message "로그 이름을 입력 받습니다"
+$LogName = Read-Host "로그 이름을 입력하세요."
+
+Write-Debug -Message "`$LogName: $LogName"
+Write-Verbose -Message "'$ComputerName' 에서 '$LogName' 로그 추출 시작."
 
 Get-EventLog -LogName Security -ComputerName $ComputerName | 
 Where-Object EventID -eq $EventID |
