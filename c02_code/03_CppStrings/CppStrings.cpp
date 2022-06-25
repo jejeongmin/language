8cf0785b5acf1eb442d59ce7d7e82824317a9067
je jeongmin <jjmyann@hanmail.net>
2022-06-25 11:27:05 +0900

    string 객체 관련 코드 업데이트

--- c02_code/03_CppStrings/CppStrings.cpp
+++ c02_code/03_CppStrings/CppStrings.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+const char* return_const_string_ptr();
+
 int main()
 {
 	string myString = "hello";
@@ -18,5 +20,22 @@ int main()
 	cout << myString << endl;
 	cout << myOtherString << endl;
 
+	const char* invalid_ptr = return_const_string_ptr();
+	cout << invalid_ptr << endl;
+
+	auto	string1 = "hello world";
+	auto	string2 = "hello world"s;
+
 	return 0;
+}
+
+/*
+	tempstring 은 지역 변수인데, 그 지역 변수에 대한 const char* 를 반환하면 호출한 쪽에서 받을 때는
+	당연히 접근하면 안되는 쓰레기 값이 들어있지. 이런 식으로 코드 짤 일은 드물겠지만, 눈에 잘 안보일 수 있으니 주의.
+*/
+const char* return_const_string_ptr()
+{
+	std::string		tempstring = "valid in return_const_string_ptr()";
+
+	return tempstring.c_str();
 }
--- c02_code/05_StringViews/StringViews.cpp
+++ c02_code/05_StringViews/StringViews.cpp
@@ -5,6 +5,13 @@
 
 using namespace std;
 
+/*
+	함수의 인자로 string literal 을 전달하면, 컴파일러는 그 리터럴의 복사본이 담긴 string 객체를
+	생성해서 함수에 전달하기 때문에 오버헤드가 발생한다. 간혹 이러한 함수에 대해 오버로딩 버전을 여러개 만들기도 하는데 좋은 선택은 아니다.
+	c++17 부터 추가된 string_view 클래스를 사용하면 이러한 고민을 해결할 수 있다.
+	아무런 성능 오버 헤드 없이 string 객체의 함수를 거의 유사하게 사용할 수 있다.
+	단, string 과 string_view 가 완전히 동일하지는 않아, 두 객체를 순정 그대로 서로 연결/결합할 순 없다.
+*/
 string_view extractExtension(string_view fileName)
 {
 	return fileName.substr(fileName.rfind('.'));
