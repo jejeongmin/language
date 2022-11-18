18739c1347944b31a7a126608c707263d3af7f0d
je jeongmin <jjmyann@hanmail.net>
2022-11-18 23:34:20 +0900

    chapter 30 code update

--- c30_code/05_JNI/HelloCpp.java
+++ c30_code/05_JNI/HelloCpp.java
@@ -1,3 +1,19 @@
+/*
+	0. jre 를 설치해서
+
+	1. javac HelloCpp.java 를 컴파일한다.
+
+	2. javah HelloCpp 를 해서 HelloCpp.h 파일을 생성한다.
+
+	3. HelloCpp.cpp 파일을 구현한다.
+
+	JNI ( Java Native Interface ) 는 Java 코드에서 C++ 를 호출하게 해준다.
+	성능이 중요한 모듈을 c++ 로 구현해서 활용할 수 있다.
+
+	그 반대의 케이스도 가능하다. c++ 코드 내에서 Java 코드를 호출할 수 있다.
+	이렇게 쓰는 용례는 거의 들어본 적이 없어서 뭐에 활용할 수 있을 지 모르겠다.
+*/
+
 public class HelloCpp {
 
     static {
--- c30_code/06_Perl/encryptString.cpp
+++ c30_code/06_Perl/encryptString.cpp
@@ -4,6 +4,12 @@
 
 using namespace std;
 
+/*
+	이 예제를 실행하려면 encryptString.cpp를 encryptString란 이름으로 컴파일하고 다음과 같이 펄 스크립트를 실행한다.
+
+	> perl processLog.pl
+*/
+
 string encrypt(string_view input);
 
 int main(int argc, char* argv[])
