70c5d32c511d96ce79edf2a7e37f33402f4280aa
je jeongmin <jjmyann@hanmail.net>
2023-08-14 17:59:03 +0900

    rwlock upload

---
+++ RWLockTest/RWLock.h
@@ -0,0 +1,29 @@
+#pragma once
+
+class RWLock
+{
+public:
+    RWLock();
+    virtual ~RWLock();
+
+    RWLock(const RWLock& rhs) = delete;
+    RWLock& operator=(const RWLock& rhs) = delete;
+
+    /// exclusive mode
+    void EnterWriteLock();
+    void LeaveWriteLock();
+
+    /// shared mode
+    void EnterReadLock();
+    void LeaveReadLock();
+
+    long GetLockFlag() const { return mLockFlag; }
+private:
+    enum LockFlag
+    {
+        LF_WRITE_MASK = 0x7FF00000,
+        LF_WRITE_FLAG = 0x00100000,
+        LF_READ_MASK = 0x000FFFFF ///< 하위 20비트를 readlock을 위한 플래그로 사용한다.
+    };
+    volatile long mLockFlag;
+};
